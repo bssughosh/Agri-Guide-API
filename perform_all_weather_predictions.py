c8bc81604294cd28dd3ceb39e47b16f8fc9d8ea3
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-02-02 23:18:04 +0530

    Added code to run all weather predictions

---
+++ perform_all_weather_predictions.py
@@ -0,0 +1,41 @@
+import os
+
+import pandas as pd
+
+from humidity_predictions import humidity_caller
+from rainfall_predictions import rain_caller
+from temp_predictions import temperature_caller
+
+# import multiprocessing
+
+base_url = 'https://raw.githubusercontent.com/bssughosh/agri-guide-data/master/datasets/weather/'
+places_file = 'places.csv'
+
+places = pd.read_csv(base_url + places_file)
+
+all_states = list(places['State'])
+all_dists = list(places['District'])
+
+files1 = os.listdir('outputs/temp')
+files2 = os.listdir('outputs/humidity')
+files3 = os.listdir('outputs/rainfall')
+
+for state, dist in zip(all_states, all_dists):
+    try:
+        file = dist + ',' + state + '.csv'
+        if file not in files1:
+            temperature_caller(state, dist)
+
+        print(f'Temperature prediction for {dist},{state} is done')
+
+        if file not in files2:
+            humidity_caller(state, dist)
+
+        print(f'Humidity prediction for {dist},{state} is done')
+
+        if file not in files3:
+            rain_caller(state, dist)
+
+        print(f'Rainfall prediction for {dist},{state} is done')
+    except:
+        print(f'Exception occurred for {dist},{state}')
