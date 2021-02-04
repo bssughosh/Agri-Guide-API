a2e793da3fa93ea9dec99e1594ae968baf7641e2
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-02-04 23:28:22 +0530

    Added instructions.txt

--- startup_instructions.sh
+++ startup_instructions.sh
@@ -4,7 +4,6 @@ sudo apt install bzip2 libxml2-dev libsm6 libxrender1 libfontconfig1 wget
 cd
 wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.10-Linux-x86_64.sh
 bash Miniconda3-4.7.10-Linux-x86_64.sh
-read -p "Press enter to continue"
 source ~/.bashrc
 rm Miniconda3-4.7.10-Linux-x86_64.sh
 conda create -n fyp python=3.7
