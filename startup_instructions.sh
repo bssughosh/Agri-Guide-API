05530de5553bd4cc3699d1e5e2424ed69b00bf25
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-02-04 09:28:40 +0530

    Added bash script

---
+++ startup_instructions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install bzip2 libxml2-dev libsm6 libxrender1 libfontconfig1 wget
+wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.10-Linux-x86_64.sh
+bash Miniconda3-4.7.10-Linux-x86_64.sh
+source ~/.bashrc
+rm Miniconda3-4.7.10-Linux-x86_64.sh
+conda create -n fyp python=3.7
+conda activate fyp
+cd Agri-Guide-API/
+pip install -r requirements.txt
+
