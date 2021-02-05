7d2b8bd814452ffb553b63737060095470e11191
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-02-05 09:18:13 +0530

    Added exceptions.csv

--- perform_all_weather_predictions.py
+++ perform_all_weather_predictions.py
@@ -1,5 +1,6 @@
 import os
 import time
+from csv import writer
 
 import pandas as pd
 
@@ -20,6 +21,7 @@ files2 = os.listdir('outputs/humidity')
 files3 = os.listdir('outputs/rainfall')
 
 for state, dist in zip(all_states, all_dists):
+    exception_file = pd.read_csv('outputs/exceptions.csv')
     try:
         file = dist + ',' + state + '.csv'
         print(f'Started for {dist},{state}')
@@ -35,8 +37,14 @@ for state, dist in zip(all_states, all_dists):
 
         t2 = time.time()
         if file not in files3:
-            rain_caller(state, dist)
+            exception_file_1 = exception_file[exception_file['State'] == state]
+            exception_file_1 = exception_file_1[exception_file_1['District'] == dist]
+            if exception_file_1.shape[0] == 0:
+                rain_caller(state, dist)
         print(f'Rainfall prediction for {dist},{state} is done in {time.time() - t2}')
 
     except:
         print(f'Exception occurred for {dist},{state}')
+        with open('outputs/exceptions.csv', 'a+', newline='') as fd:
+            csv_writer = writer(fd)
+            csv_writer.writerow([state, dist])
